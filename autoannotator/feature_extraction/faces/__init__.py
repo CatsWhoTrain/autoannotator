78d76cd5a01d4e6233d8f99d6313eaf9139f789e
Igor <36381961+ponjoru@users.noreply.github.com>
2023-09-09 08:25:58 +0300

    +added face recognition example (#6)
    Co-authored-by: igor.popov <ig.popov1997@gmail.com>

---
+++ autoannotator/feature_extraction/faces/__init__.py
@@ -0,0 +1,3 @@
+from .models.ensemble import FaceFeatureExtractionEnsemle
+from .models.model_adaface import FaceFeatureExtractorAdaface
+from .models.model_insightface import FaceFeatureExtractorInsightface
---
+++ examples/face_recognition_example.py
@@ -0,0 +1,67 @@
+from autoannotator.types.faces import Face
+from autoannotator.utils.image_reader import ImageReader
+from autoannotator.detection.faces import SCRFD, YOLOv7, FaceDetEnsemble
+from autoannotator.utils.image_alignment import ImageAlignmentRegression
+from autoannotator.feature_extraction.faces import FaceFeatureExtractorAdaface, FaceFeatureExtractorInsightface, FaceFeatureExtractionEnsemle
+
+
+def auto_annotate_faces(img_files: List[str]) -> List[Face]:
+    """
+    Face recognition usage example
+
+    Arguments:
+        img_files: List of image paths to recognize
+
+    Returns:
+        (List[Face]) - List of recognized faces
+    """
+    # ini image file reader
+    reader = ImageReader()
+
+    # init face detector ensemble
+    models = [SCRFD(), YOLOv7()]
+    fd_ensemble = FaceDetEnsemble(models=models)
+
+    # init alignment regressor
+    regressor = ImageAlignmentRegression()
+
+    # init feature extractor ensemble
+    models = [FaceFeatureExtractorAdaface(), FaceFeatureExtractorInsightface()]
+    fr_ensemble = FaceFeatureExtractionEnsemle(models=models)
+
+    # start processing images
+    faces_arr = []
+    descriptors = []
+    for img_ind, img_file in enumerate(img_files):
+        # read image
+        img = reader(img_file)
+
+        # detect faces with ensemble
+        faces = fd_ensemble(img)
+
+        # for each detected face run alignment and face descriptor extractor
+        for face in faces:
+            # get aligned image
+            aligned_img = regressor(img, face.landmarks)
+
+            # extract descriptor with the ensemble of descriptor extractors
+            descriptor = fr_ensemble(aligned_img)
+
+            # save face descriptors for further clusterization
+            descriptors.append(descriptor)
+
+            # save faces
+            faces_arr.append(face)
+
+    # init clusterization method
+    dbscan = ClusteringDBSCAN(type="sklearn", eps=0.01, min_samples=2)
+
+    # clusterize descriptors
+    labels = dbscan(descriptors)
+
+    # set cls_id for each face
+    results = []
+    for face, label in zip(faces_arr, labels):
+        face.cls_id = label
+        results.append(face)
+    return results
