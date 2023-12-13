4905bdb83286861084864d494fcf8af36c3af9f8
igor.popov <igor.popov@intrtl.com>
2023-12-13 15:14:09 +0400

    +added IterDETR human detector

---
+++ autoannotator/utils/algorithms.py
@@ -0,0 +1,35 @@
+import numpy as np
+from autoannotator.types.base import Detection
+from typing import List
+
+
+def non_maximum_suppression(detections: List[Detection], iou_thresh=0.5) -> List[int]:
+    bboxes = np.array([d.bbox for d in detections])
+    scores = np.array([d.score for d in detections])
+
+    x1 = bboxes[:, 0]
+    y1 = bboxes[:, 1]
+    x2 = bboxes[:, 2]
+    y2 = bboxes[:, 3]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= iou_thresh)[0]
+        order = order[inds + 1]
+
+    return keep
